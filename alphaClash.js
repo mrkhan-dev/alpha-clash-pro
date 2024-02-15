42ac6f1bff131dfc4ad3cfc8baaab7ad6ec9b20f
mrkhan-dev <sk5094625@gmail.com>
2024-02-15 22:45:24 +0600

    added some js code

---
+++ alphaClash.js
@@ -0,0 +1,14 @@
+// function play() {
+//   // hide home section
+//   const homeSection = document.getElementById("home");
+//   homeSection.classList.add("hidden");
+
+//   // show playground section
+//   const playgroundSection = document.getElementById("playground");
+//   playgroundSection.classList.remove("hidden");
+// }
+
+function play() {
+  hideElementById("home");
+  showElementById("playground");
+}
